44391d7520d95fd84380aa3dfef82dc404bd098e
Shivam Kumar Gupta <159278628+shivam15102005@users.noreply.github.com>
2025-08-24 10:35:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,14 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const incomeRoutes = require("./routes/incomeRoutes"); 
-const expenseRoutes = require("./routes/expenseRoutes");
-const  dashboardRoutes = require("./routes/dashboardRoutes");
+import 'dotenv/config';  // dotenv auto config
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import incomeRoutes from './routes/incomeRoutes.js';
+import expenseRoutes from './routes/expenseRoutes.js';
+import dashboardRoutes from './routes/dashboardRoutes.js';
 
 
 const app = express();
@@ -34,7 +35,9 @@ app.use("/api/v1/dashboard", require("./routes/dashboardRoutes"));
 // Serve static files from the uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // This handles all other routes and serves index.html
@@ -45,6 +48,7 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 
 
 
