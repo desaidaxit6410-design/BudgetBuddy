7304d7c75fdefd4223dcefc31e50a3dd905991e4
Shivam Kumar Gupta <159278628+shivam15102005@users.noreply.github.com>
2025-08-24 12:34:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const incomeRoutes = require('./routes/incomeRoutes');
-const expenseRoutes = require('./routes/expenseRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes"); 
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to handle CORS
+// Middleware to handle cors
 app.use(cors({
   origin: process.env.CLIENT_URL || "*",
-  methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+  allowedHeaders:["Content-Type", "Authorization"] ,
+
 }));
 
 // Middleware to parse JSON requests
@@ -24,21 +25,15 @@ app.use(express.json());
 
 connectDB();
 
-// API routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
-// Serve uploads folder
+// Serve static files from the uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Serve React SPA (assuming build folder is 'dist')
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
