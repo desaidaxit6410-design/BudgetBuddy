1a6924135a5973e17f20ee3327e57c3afe5a2789
Shivam Kumar Gupta <159278628+shivam15102005@users.noreply.github.com>
2025-08-26 21:20:22 +0530

    Update apiPaths.js

--- Frontend/MySpend/src/Utils/apiPaths.js
+++ Frontend/MySpend/src/Utils/apiPaths.js
@@ -7,6 +7,7 @@ export const API_PATHS = {
         LOGIN: "/api/v1/auth/login",
         REGISTER: "/api/v1/auth/register",
         GET_USER_INFO: "/api/v1/auth/getUser",
+        UPDATE: "/api/v1/auth/update-profile",
     },
     DASHBOARD: {
         GET_DATA: "/api/v1/dashboard",
