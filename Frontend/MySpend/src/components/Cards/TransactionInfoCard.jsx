f46fa1070d1a671e112e4e08ba49dee215393d17
Shivam Kumar Gupta <159278628+shivam15102005@users.noreply.github.com>
2025-08-27 22:52:28 +0530

    Update TransactionInfoCard.jsx

--- Frontend/MySpend/src/components/Cards/TransactionInfoCard.jsx
+++ Frontend/MySpend/src/components/Cards/TransactionInfoCard.jsx
@@ -30,7 +30,9 @@ const TransactionInfoCard = ({ title, icon, date, amount, type, hideDeleteBtn, o
                 <div className='flex items-center gap-2'>
                     {!hideDeleteBtn && (
                         <button
-                            className='text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer'
+                            className='text-gray-400 hover:text-red-500 
+                                       opacity-100 md:opacity-0 md:group-hover:opacity-100 
+                                       transition-opacity cursor-pointer'
                             onClick={onDelete}
                         >
                             <LuTrash2 size={18} />
